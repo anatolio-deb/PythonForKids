ae48bb8224d60d94d20f5801b6131bd0bd4ecf00
goeth <43092835+goeth@users.noreply.github.com>
2020-02-01 00:12:27 +0500

    Update try-to-open.py

--- Игра «Открой сундук»/try-to-open.py
+++ Игра «Открой сундук»/try-to-open.py
@@ -6,31 +6,34 @@ password = []
 password_match = []
 
 for i in range(2):
-    password.append(str(random.randint(1,5))) # Запираем сундук случайным поролем
-print(password) # показать пароль (чит)
+  password.append(str(random.randint(1,5))) # Запираем сундук случайным поролем
+#print(password) # показать пароль (чит)
+
 '''
-# Если не понятно, сгенерируем пароль по-другому:
-a = str(random.randint(0,5))
-b = str(random.randint(0,5))
+# Если не понятно, сгенерируем пароль по-другому.
+a = str(random.randint(1,5))
+b = str(random.randint(1,5))
 password = a + b
 # В итоге получим строку из двух значений.
 '''
-while master_keys > 0 and password != password_match:
-    password_match = []
-    for k in input('Сундук закрыт, попробуй взломать:\n'):
-        password_match.append(k)
-    '''
-    # Если не понятно, примем ввод по-другому:
-    a = str(input("Введите первую цифру пароля: ))
-    b = str(input("Введите вторую цифру пароля: ))
-    password_match = a + b
-    # В итоге получим строку из двух значений.
-    '''
-    master_keys -= 1
+
+while master_keys != 0:
+  for k in input('Сундук закрыт, попробуй взломать:\n'):
+    password_match.append(k)
+  '''
+  # Если не понятно, примем ввод по-другому.
+  a = str(input("Введите первую цифру пароля: ))
+  b = str(input("Введите вторую цифру пароля: ))
+  password_match = a + b
+  # В итоге получим строку из двух значений.
+  '''
+ 
+  if password == password_match:
+    item = random.choice(items) # Ложим случайный предмет в сундук
+    print("Ты подобрал", item, ", поздравляем!")
+    break
+  else:
+    master_keys = master_keys-1
     print('Ой! Осталось', master_keys, 'отмычек.')
-else:
-    if password == password_match:
-        item = random.choice(items) # Ложим случайный предмет в сундук
-        print("Ты подобрал", item, ", поздравляем!")
-    else:
-        print('Приходи с новыми отмычками!')
+    if master_keys == 0:
+      print('Приходи с новыми отмычками!')
