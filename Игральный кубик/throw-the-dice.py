41d3ed9519b27717140ced1acafcc25320086a9b
BuildTools <unconfigured@null.spigotmc.org>
2020-02-05 11:17:43 +0500

    First commit

---
+++ Игральный кубик/throw-the-dice.py
@@ -0,0 +1,35 @@
+import random
+import time
+# Два пустых списка для хранения очков игроков
+player1, player2 = [],[]
+# Счетчик бросков первого игрока - без него игроки "бросают" поочереди 
+count = 0
+# Три броска на каждого игрока
+for i in range(3):
+	# Пока первый игрок не бросит три раза, не переходить ко второму игроку
+	while count < 3:
+		print('Первый игрок бросает...')
+		shot1 = int(random.randint(1,6))
+		player1.append(shot1)
+		# Пауза для реалистичности броска кубика
+		time.sleep(3)
+		print('Выпало:', shot1)
+		count += 1
+	print('Второй игрок бросает...')
+	shot2 = int(random.randint(1,6))
+	player2.append(shot2)
+	time.sleep(3)
+	print('Выпало:', shot2)
+# Подсчет суммы очков в списках
+player1 = player1[0] + player1[1] + player1[2]
+player2 = player2[0] + player2[1] + player2[2]
+
+print('Первый игрок набрал:', player1, 'очков')
+print('Второй игрок набрал:', player2, 'очков')
+
+if player1 > player2:
+	print('Победил первый игрок.')
+elif player1 < player2:
+	print('Победил второй игрок.')
+else:
+	print('Ничья!')
